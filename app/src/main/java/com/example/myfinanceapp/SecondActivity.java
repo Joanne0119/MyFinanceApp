bdaf9ade12f8cf3ed70746c59de33a122a8ab8db
劉丞恩 <114967562+Joanne0119@users.noreply.github.com>
2024-12-13 14:03:29 +0800

    add spinner to setting activity_second.xml

--- app/src/main/java/com/example/myfinanceapp/SecondActivity.java
+++ app/src/main/java/com/example/myfinanceapp/SecondActivity.java
@@ -1,13 +1,22 @@
 package com.example.myfinanceapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -24,9 +33,11 @@ public class SecondActivity extends AppCompatActivity {
                 goBack(v);
             }
         });
+
     }
 
     public void goBack(View v) {
         finish(); // 結束 Activity, 即可回到前一個 Activity
     }
+
 }
